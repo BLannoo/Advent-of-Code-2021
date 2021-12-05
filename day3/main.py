8fcd235e46493680a91da6451ec54e5dadaf52a3
Bruno Lannoo <Bruno.Lannoo@nike.com>
2021-12-05 23:24:38 +0100

    Day 3 gold

--- day3/main.py
+++ day3/main.py
@@ -1,22 +1,49 @@
 from pathlib import Path
-from typing import List
+from typing import List, Callable
 
 
 def silver(input_file_path: Path) -> int:
     diagnostic_report = input_file_path.read_text().split("\n")
 
-    gamma_rate = rate(diagnostic_report, least_common_bit)
-    epsilon_rate = rate(diagnostic_report, most_common_bit)
+    gamma_rate = silver_rate(diagnostic_report, least_common_bit)
+    epsilon_rate = silver_rate(diagnostic_report, most_common_bit)
 
     return gamma_rate * epsilon_rate
 
 
-def rate(diagnostic_report, bit_selector):
+def gold(input_file_path: Path) -> int:
+    diagnostic_report = input_file_path.read_text().split("\n")
+
+    oxygen_generator_rating = gold_rate(diagnostic_report, most_common_bit)
+    co2_scrubber_rating = gold_rate(diagnostic_report, least_common_bit)
+
+    return oxygen_generator_rating * co2_scrubber_rating
+
+
+def silver_rate(
+    diagnostic_report: List[str],
+    bit_selector: Callable[[List[str], int], int]
+) -> int:
     bits = [
-        bit_selector(diagnostic_report, at=i)
+        bit_selector(diagnostic_report, i)
         for i in range(len(diagnostic_report[0]))
     ]
     return binary_to_int(bits)
+
+
+def gold_rate(
+    diagnostic_report: List[str],
+    bit_selector: Callable[[List[str], int], int]
+) -> int:
+    for i in range(len(diagnostic_report)):
+
+        if len(diagnostic_report) == 1:
+            return int(diagnostic_report[0], 2)
+
+        diagnostic_report = list(filter(
+            lambda binary: int(binary[i]) == bit_selector(diagnostic_report, i),
+            diagnostic_report
+        ))
 
 
 def most_common_bit(diagnostic_report: List[str], at: int) -> int:
@@ -29,7 +56,10 @@ def least_common_bit(diagnostic_report: List[str], at: int) -> int:
         for binary_number in diagnostic_report
     ]
 
-    return len(diagnostic_report) // sum(bits) - 1
+    ones = sum(bits)
+    zeros = len(diagnostic_report) - ones
+
+    return int(ones < zeros)
 
 
 def binary_to_int(least_common_bits: List[int]) -> int:
--- day3/test.py
+++ day3/test.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from day3.main import silver
+from day3.main import silver, gold
 
 INPUT_PATH = Path(__file__).parent / "input.txt"
 EXAMPLE_PATH = Path(__file__).parent / "example.txt"
@@ -12,3 +12,11 @@ def test_silver_example():
 
 def test_silver_star():
     assert silver(INPUT_PATH) == 749376
+
+
+def test_gold_example():
+    assert gold(EXAMPLE_PATH) == 230
+
+
+def test_gold_star():
+    assert gold(INPUT_PATH) == 2372923
