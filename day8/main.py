e5a9c3b124f41a5a0a1cae5bf69109d62e8d974c
Bruno Lannoo <Bruno.Lannoo@nike.com>
2021-12-12 16:18:26 +0100

    Day 8 gold

--- day8/main.py
+++ day8/main.py
@@ -1,5 +1,20 @@
+from collections import Counter
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
+
+PATTERN_TO_DIGIT = {
+    "abcefg": "0",
+    "cf": "1",
+    "acdeg": "2",
+    "acdfg": "3",
+    "bcdf": "4",
+    "abdfg": "5",
+    "abdefg": "6",
+    "acf": "7",
+    "abcdefg": "8",
+    "abcdfg": "9",
+}
 
 
 @dataclass(frozen=True)
@@ -7,10 +22,20 @@ class Entry:
     raw: str
 
     @property
-    def outputs(self):
-        return self.raw.split(" | ")[1].split(" ")
+    def outputs(self) -> List[str]:
+        return [
+            ''.join(sorted(output))
+            for output in self.raw.split(" | ")[1].split(" ")
+        ]
 
-    def count_output_1478(self):
+    @property
+    def patterns(self) -> List[str]:
+        return [
+            ''.join(sorted(pattern))
+            for pattern in self.raw.split(" | ")[0].split(" ")
+        ]
+
+    def count_output_1478(self) -> int:
         return len(
             [
                 output
@@ -19,11 +44,102 @@ class Entry:
             ]
         )
 
+    def output_value(self) -> int:
+        return int(
+            "".join(
+                [
+                    self.decode(output)
+                    for output in self.outputs
+                ]
+            )
+        )
+
+    def get_digit_pattern(self, digit: int) -> str:
+        return {
+            1: self._get_digit_pattern_by_length(2),
+            4: self._get_digit_pattern_by_length(4),
+            7: self._get_digit_pattern_by_length(3),
+            8: self._get_digit_pattern_by_length(7),
+        }[digit]
+
+    def _get_digit_pattern_by_length(self, length: int) -> str:
+        return [
+            pattern
+            for pattern in self.patterns
+            if len(pattern) == length
+        ][0]
+
+    def get_actual_segment(self, displayed_segment):
+        occurrences_map = {
+            self._get_segments_by_occurrence(6)[0]: "b",
+            self._get_segments_by_occurrence(4)[0]: "e",
+            self._get_segments_by_occurrence(9)[0]: "f",
+        }
+
+        occurrence_8 = self._get_segments_by_occurrence(8)
+        if occurrence_8[0] in self.get_digit_pattern(1):
+            occurrence_8_map = {
+                occurrence_8[0]: "c",
+                occurrence_8[1]: "a",
+            }
+        else:
+            occurrence_8_map = {
+                occurrence_8[0]: "a",
+                occurrence_8[1]: "c",
+            }
+
+        occurrence_7 = self._get_segments_by_occurrence(7)
+        if occurrence_7[0] in self.get_digit_pattern(4):
+            occurrence_7_map = {
+                occurrence_7[0]: "d",
+                occurrence_7[1]: "g",
+            }
+        else:
+            occurrence_7_map = {
+                occurrence_7[0]: "g",
+                occurrence_7[1]: "d",
+            }
+
+        return {
+            **occurrences_map,
+            **occurrence_8_map,
+            **occurrence_7_map,
+        }.get(displayed_segment, "z")
+
+    def _get_segments_by_occurrence(self, occurrence: int) -> List[str]:
+        occurrences = Counter(self.raw.split(" | ")[0].replace(" ", ""))
+
+        return [
+            k
+            for k, v in occurrences.items()
+            if v == occurrence
+        ]
+
+    def decode(self, output: str) -> str:
+        actual_pattern = "".join(
+            sorted(
+                [
+                    self.get_actual_segment(displayed_segment)
+                    for displayed_segment in output
+                ]
+            )
+        )
+        return PATTERN_TO_DIGIT[actual_pattern]
+
 
 def silver(input_file_path: Path) -> int:
     return sum(
         [
             Entry(raw_entry).count_output_1478()
+            for raw_entry in input_file_path.read_text().split("\n")
+        ]
+    )
+
+
+def gold(input_file_path: Path) -> int:
+    return sum(
+        [
+            Entry(raw_entry).output_value()
             for raw_entry in input_file_path.read_text().split("\n")
         ]
     )
--- day8/test.py
+++ day8/test.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from day8.main import silver
+import pytest
+
+from day8.main import silver, gold, Entry
 
 INPUT_PATH = Path(__file__).parent / "input.txt"
 EXAMPLE_PATH = Path(__file__).parent / "example.txt"
@@ -12,3 +14,54 @@ def test_silver_example():
 
 def test_silver_star():
     assert silver(INPUT_PATH) == 264
+
+
+EXAMPLE_ENTRY = Entry("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+
+
+@pytest.mark.parametrize(
+    "pattern,digit",
+    [
+        ("acedgfb", 8),
+        # ("cdfbe", 5),
+        # ("gcdfa", 2),
+        # ("fbcad", 3),
+        ("dab", 7),
+        # ("cefabd", 9),
+        # ("cdfgeb", 6),
+        ("eafb", 4),
+        # ("cagedb", 0),
+        ("ab", 1),
+    ]
+)
+def test_get_digit_pattern_by_length(pattern: str, digit: int):
+    sorted_pattern = "".join(sorted(pattern))
+    assert EXAMPLE_ENTRY.get_digit_pattern(digit) == sorted_pattern
+
+
+@pytest.mark.parametrize(
+    "actual_segment,displayed_segment",
+    [
+        ("a", "d"),
+        ("b", "e"),
+        ("c", "a"),
+        ("d", "f"),
+        ("e", "g"),
+        ("f", "b"),
+        ("g", "c"),
+    ]
+)
+def test_get_actual_segment(actual_segment: str, displayed_segment: str):
+    assert EXAMPLE_ENTRY.get_actual_segment(displayed_segment) == actual_segment
+
+
+def test_output_value():
+    assert EXAMPLE_ENTRY.output_value() == 5353
+
+
+def test_gold_example():
+    assert gold(EXAMPLE_PATH) == 61229
+
+
+def test_gold_star():
+    assert gold(INPUT_PATH) == 1063760
