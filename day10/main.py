04da6d1824958287f3959abe9aab799660682fed
Bruno Lannoo <Bruno.Lannoo@nike.com>
2021-12-15 15:31:54 +0100

    Day 10 gold

--- day10/main.py
+++ day10/main.py
@@ -20,6 +20,22 @@ def silver(input_file_path: Path) -> int:
     )
 
 
+def gold(input_file_path: Path) -> int:
+    lines = input_file_path.read_text().split("\n")
+
+    completion_scores = [
+        completion_scoring(find_completion(line))
+        for line in lines
+        if first_corrupted_character(line) == ""
+    ]
+
+    completion_scores.sort()
+
+    middle_index = int((len(completion_scores)) / 2)
+
+    return completion_scores[middle_index]
+
+
 BRACES = (
     "()",
     "[]",
@@ -36,9 +52,7 @@ def contains_consecutive_match(line: str) -> bool:
 
 
 def first_corrupted_character(line: str) -> str:
-    while contains_consecutive_match(line):
-        for brace_pair in BRACES:
-            line = line.replace(brace_pair, "")
+    line = remove_matching(line)
 
     first_closing = [
         line.find(brace_close)
@@ -50,3 +64,41 @@ def first_corrupted_character(line: str) -> str:
         return ""
 
     return line[min(first_closing)]
+
+
+def remove_matching(line):
+    while contains_consecutive_match(line):
+        for brace_pair in BRACES:
+            line = line.replace(brace_pair, "")
+    return line
+
+
+def close(brace: str) -> str:
+    for brace_open, brace_close in BRACES:
+        if brace == brace_open:
+            return brace_close
+    raise Exception(f"brace '{brace}' not matched")
+
+
+def find_completion(line: str) -> str:
+    line = remove_matching(line)
+    return "".join(
+        close(brace)
+        for brace in line[::-1]
+    )
+
+
+COMPLETION_POINTS = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+def completion_scoring(completion: str) -> int:
+    score = 0
+    for brace in completion:
+        score *= 5
+        score += COMPLETION_POINTS[brace]
+    return score
--- day10/test.py
+++ day10/test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from day10.main import silver, first_corrupted_character
+from day10.main import silver, first_corrupted_character, gold, find_completion, completion_scoring
 
 INPUT_PATH = Path(__file__).parent / "input.txt"
 EXAMPLE_PATH = Path(__file__).parent / "example.txt"
@@ -29,3 +29,39 @@ def test_silver_example():
 
 def test_silver_star():
     assert silver(INPUT_PATH) == 323613
+
+
+@pytest.mark.parametrize(
+    "line,completion",
+    [
+        ("[({(<(())[]>[[{[]{<()<>>", "}}]])})]"),
+        ("[(()[<>])]({[<{<<[]>>(", ")}>]})"),
+        ("(((({<>}<{<{<>}{[]{[]{}", "}}>}>))))"),
+        ("{<[[]]>}<{[{[{[]{()[[[]", "]]}}]}]}>"),
+        ("<{([{{}}[<[[[<>{}]]]>[]]", "])}>"),
+    ]
+)
+def test_completion(line: str, completion: str):
+    assert find_completion(line) == completion
+
+
+@pytest.mark.parametrize(
+    "completion,score",
+    [
+        ("}}]])})]", 288957),
+        (")}>]})", 5566),
+        ("}}>}>))))", 1480781),
+        ("]]}}]}]}>", 995444),
+        ("])}>", 294),
+    ]
+)
+def test_completion_scoring(completion: str, score: int):
+    assert completion_scoring(completion) == score
+
+
+def test_gold_example():
+    assert gold(EXAMPLE_PATH) == 288957
+
+
+def test_gold_star():
+    assert gold(INPUT_PATH) == 3103006161
